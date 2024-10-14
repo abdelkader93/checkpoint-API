47010bc2970604f6341a78db91a0dff452e892c7
Abdelkader.ferchichi <abdelkader.ferchichi@esprit.tn>
2024-10-14 11:15:09 +0100

    checkpoint API

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Users from './components/Users';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> List of users</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
